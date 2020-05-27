75f54c43f5453959d898f935ec6bc7ee51036db4
zagan-the-gun <zagan.the.gun@gmail.com>
2020-05-27 11:36:40 +0900

    自動ツイート機能実装

--- apps/management/commands/tweet_schedule.py
+++ apps/management/commands/tweet_schedule.py
@@ -7,7 +7,7 @@ import tweepy
 
 
 class Command(BaseCommand):
-    help = 'マスク情報収集'
+    help = 'ツイート！'
 
 #    def add_arguments(self, parser):
 #        parser.add_argument('-c', '--command', dest='command', help='add or del')
@@ -20,17 +20,19 @@ class Command(BaseCommand):
         self.stdout.write(self.style.SUCCESS('DEBUG DEBUG DEBUG tweet_schedule: '))
 #        self.stdout.write(options.__str__())
 
-        START_DATE = datetime.datetime.strptime('2020-01-01 00:00:00', '%Y-%m-%d %H:%M:%S').replace(tzinfo=pytz.timezone('Asia/Tokyo'))
-        END_DATE = datetime.datetime.now(pytz.timezone('Asia/Tokyo'))
+        START_DATE = pytz.timezone('Asia/Tokyo').localize(datetime.datetime.strptime('2020-01-01 00:00:00', '%Y-%m-%d %H:%M:%S').replace())
+        END_DATE = pytz.timezone('Asia/Tokyo').localize(datetime.datetime.now())
 
         tweet_schedule = TweetSchedule.objects.filter(tweet_at__range=(START_DATE, END_DATE), tweeted=False, active=True).order_by('-tweet_at')
         for t in tweet_schedule:
-            auth = tweepy.OAuthHandler(t.tweet_account.consumer_api_key, t.tweet_account.consumer_api_secret_key)
-            auth.set_access_token(t.tweet_account.access_token, t.tweet_account.access_token_secret)
-            api = tweepy.API(auth)
-            api.update_status(status=t.tweet_content)
-            t.tweeted=True
-            t.save()
-
-            continue
+            try:
+                auth = tweepy.OAuthHandler(t.tweet_account.consumer_api_key, t.tweet_account.consumer_api_secret_key)
+                auth.set_access_token(t.tweet_account.access_token, t.tweet_account.access_token_secret)
+                api = tweepy.API(auth)
+                api.update_status(status=t.tweet_content)
+                t.tweeted=True
+#                t.save()
+            except Exception as e:
+                print(e)
+                continue
 
